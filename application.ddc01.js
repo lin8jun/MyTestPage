ba981671eb68c93f07d6d228088cbdeadebd2fd7
JohnsonLin <24482368@qq.com>
2026-04-16 16:55:20 +0800

    no message

--- application.ddc01.js
+++ application.ddc01.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.6b388.json';
+          this.settingsPath = 'src/settings.888d6.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
