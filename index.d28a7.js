30103ce94c1aa3c375c1aadba5ad1ea35440adf8
JohnsonLin <24482368@qq.com>
2026-04-17 19:00:21 +0800

    no message

--- index.d28a7.js
+++ index.d28a7.js
@@ -1,4 +1,4 @@
-System.register(["./application.789e3.js"], function (_export, _context) {
+System.register(["./application.87199.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
