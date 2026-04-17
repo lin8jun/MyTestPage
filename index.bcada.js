513236d558e4a9e2cc7f8fb351af483a3b5fb021
JohnsonLin <24482368@qq.com>
2026-04-17 16:40:23 +0800

    no message

--- index.bcada.js
+++ index.bcada.js
@@ -1,4 +1,4 @@
-System.register(["./application.a6b7c.js"], function (_export, _context) {
+System.register(["./application.36c27.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
