8e1156bffc9aeb8a0647e34b1a21fbb35b9bf3ef
JohnsonLin <24482368@qq.com>
2026-04-20 18:52:52 +0800

    no message

--- index.fbc6b.js
+++ index.fbc6b.js
@@ -1,4 +1,4 @@
-System.register(["./application.f1ed1.js"], function (_export, _context) {
+System.register(["./application.afeb9.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
