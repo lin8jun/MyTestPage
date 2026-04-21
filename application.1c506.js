2a6cd75a602485b5a4d7cf163b54b79c93ef321f
JohnsonLin <24482368@qq.com>
2026-04-21 20:22:40 +0800

    no message

--- application.1c506.js
+++ application.1c506.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.77a97.json';
+          this.settingsPath = 'src/settings.c186f.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- index.555d2.js
+++ index.555d2.js
@@ -1,4 +1,4 @@
-System.register(["./application.0fbca.js"], function (_export, _context) {
+System.register(["./application.1c506.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
