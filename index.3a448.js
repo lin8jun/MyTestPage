656cd259c67f5321e31b3d1ad499f55c8485a6e2
JohnsonLin <24482368@qq.com>
2026-04-21 10:02:35 +0800

    no message

--- index.3a448.js
+++ index.3a448.js
@@ -1,4 +1,4 @@
-System.register(["./application.8410c.js"], function (_export, _context) {
+System.register(["./application.511fc.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
