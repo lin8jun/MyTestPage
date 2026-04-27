24f069e955348163abe07a77e6c2608747a0850f
JohnsonLin <24482368@qq.com>
2026-04-27 14:16:01 +0800

    no message

--- index.211a2.js
+++ index.211a2.js
@@ -1,4 +1,4 @@
-System.register(["./application.fcfe8.js"], function (_export, _context) {
+System.register(["./application.fb324.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
