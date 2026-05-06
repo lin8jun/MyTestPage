d5986574e63525035e96fbb61082c0da3dec2bcd
JohnsonLin <24482368@qq.com>
2026-05-06 19:07:02 +0800

    no message

--- index.42ec6.js
+++ index.42ec6.js
@@ -1,4 +1,4 @@
-System.register(["./application.c55cf.js"], function (_export, _context) {
+System.register(["./application.fefa4.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
