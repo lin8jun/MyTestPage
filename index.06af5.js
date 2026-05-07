430eb69c86134d631ee27f6b38fb23fa1e614bab
JohnsonLin <24482368@qq.com>
2026-05-07 19:59:37 +0800

    no message

--- index.06af5.js
+++ index.06af5.js
@@ -1,4 +1,4 @@
-System.register(["./application.c3aa6.js"], function (_export, _context) {
+System.register(["./application.43407.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
