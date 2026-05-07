16fdc0da3128dc0605cf98ca981b79bffadec91a
JohnsonLin <24482368@qq.com>
2026-05-07 20:20:51 +0800

    no message

--- application.3445e.js
+++ application.3445e.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.70b45.json';
+          this.settingsPath = 'src/settings.82edf.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
