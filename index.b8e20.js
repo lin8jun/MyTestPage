63ebcb96d3bfe5a9ec8c4ac561b2f3898ac0a65e
JohnsonLin <24482368@qq.com>
2026-05-07 16:19:43 +0800

    no message

--- index.b8e20.js
+++ index.b8e20.js
@@ -1,4 +1,4 @@
-System.register(["./application.68601.js"], function (_export, _context) {
+System.register(["./application.56c34.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
