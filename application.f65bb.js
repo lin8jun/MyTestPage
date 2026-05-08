28873d8ec8cad431235b5b7bdcb8a01168f2ce4b
JohnsonLin <24482368@qq.com>
2026-05-08 10:55:03 +0800

    no message

--- application.f65bb.js
+++ application.f65bb.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.cafbc.json';
+          this.settingsPath = 'src/settings.c79ae.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
