9d2845d3b2646f97664e05e89ef0f667ea0722fa
JohnsonLin <24482368@qq.com>
2026-05-13 17:48:58 +0800

    no message

--- application.d35e9.js
+++ application.d35e9.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.f6cac.json';
+          this.settingsPath = 'src/settings.3d229.json';
           this.showFPS = true;
         }
         _createClass(Application, [{
--- index.b3e7b.js
+++ index.b3e7b.js
@@ -1,4 +1,4 @@
-System.register(["./application.1f4c9.js"], function (_export, _context) {
+System.register(["./application.d35e9.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
