698a0489e135c81760ccb5fc1ad0f1f779cf5c7f
JohnsonLin <24482368@qq.com>
2026-05-18 20:54:15 +0800

    no message

--- index.4b903.js
+++ index.4b903.js
@@ -1,4 +1,4 @@
-System.register(["./application.8e0ad.js"], function (_export, _context) {
+System.register(["./application.3fe1d.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
